95ed1486b82a2ac59f5c2dfb4c525f0a52abe8f6
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-09-19 15:50:50 +0500

    Initial Commit

---
+++ event-bus/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post('/events', (req, res) => {
+	const event = req.body;
+
+	events.push(event);
+
+	// Posts service
+	axios.post('http://localhost:4000/events', event).catch(err => {
+		console.log(err.message);
+	});
+	// Comments Service
+	axios.post('http://localhost:4001/events', event).catch(err => {
+		console.log(err.message);
+	});
+	// Query Service
+	axios.post('http://localhost:4002/events', event).catch(err => {
+		console.log(err.message);
+	});
+	// Moderation Service
+	axios.post('http://localhost:4003/events', event).catch(err => {
+		console.log(err.message);
+	});
+	res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+	res.send(events);
+});
+
+app.listen(4005, () => {
+	console.log('EVENT BUS Service is listening on 4005');
+});
